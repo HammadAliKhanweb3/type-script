9755953e3b6b175d16a5c3a4162d69d393eec651
Hammad Ali Khan <hammad.web3.0developer@gmail.com>
2025-10-17 20:17:27 +0500

    created a expresss server

--- typescriptWithBun/index.ts
+++ typescriptWithBun/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+
+
+const app = express()
+
+
+
+
+
+app.get("/",(req,res)=>{
+
+    res.json({"server:":"is running fine."})
+})
+
+
+
+app.listen("3000",()=>{
+    console.log("server is running on port 3000");
+    
+})
