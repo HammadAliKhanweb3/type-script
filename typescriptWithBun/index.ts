6fafeb8303cf247c0a24dd57d929c92a6358bc44
Hammad Ali Khan <hammad.web3.0developer@gmail.com>
2025-10-22 22:35:14 +0500

    add setup for redux

--- typescriptWithBun/index.ts
+++ typescriptWithBun/index.ts
@@ -4,18 +4,18 @@ import express from "express";
 
 const app = express()
 
-
+const Port = 3000
 
 
 
 app.get("/",(req,res)=>{
 
-    res.json({"server:":"is running fine."})
+    res.json({server:"is running fine."})
 })
 
 
 
-app.listen("3000",()=>{
+app.listen(Port,()=>{
     console.log("server is running on port 3000");
     
 })
